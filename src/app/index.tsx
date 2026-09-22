8cff4ecdbf2f362dd1712cf4d78ab101864e9d08
Kevin Pintar <kevinpintar@hotmail.fr>
2026-09-22 10:15:39 +0200

    inital commit

---
+++ src/app/index.tsx
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-2xl font-bold text-red-500">Hello Pokémon 👋</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
